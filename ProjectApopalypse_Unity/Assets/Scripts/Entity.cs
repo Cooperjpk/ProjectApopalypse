7f7ce96d2fad6758fd3e5b12a7637df4b5d7fad8
Cooperjpk <cooperjpklassen@gmail.com>
2018-09-01 16:07:43 -0700

    Adding some new stuff :)

---
+++ ProjectApopalypse_Unity/Assets/Scripts/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Entity : MonoBehaviour
+{
+    public string displayName;
+    public string technicalName;
+    public GameObject model;
+    public Image icon;
+
+    public string[] classifications;
+    public string description;
+
+    public int powerLevel;
+    public int PowerLevel
+    {
+        get
+        {
+            //Calculate the power level of the entity here.
+            return powerLevel;
+        }
+    }
+
+    public enum Rarity
+    {
+        Common,
+        Rare,
+        Epic,
+        Legendary
+    }
+    Rarity rarity;
+
+    //Stage.Theme theme;
+
+    Animator animator;
+    Renderer renderer;
+    Texture variation;
+
+    void Start()
+    {
+    }
+}
---
+++ ProjectApopalypse_Unity/Assets/Scripts/Variable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+{
+    public int variable;
+
+    bool isLocked;
+    int minVar = 0;
+    int maxVar = 999;
+
+    public int Variable
+    {
+        get
+        {
+            return variable;
+        }
+        set
+        {
+            if (isLocked)
+            {
+                Debug.Log("This variable is locked.");
+                return variable;
+            }
+            else
+            {
+                variable = value;
+                if (variable > maxVar)
+                {
+                    variable = maxVar;
+                }
+                else if (variable < minVar)
+                {
+                    variable = minVar;
+                }
+            }
+        }
+    }
+}
